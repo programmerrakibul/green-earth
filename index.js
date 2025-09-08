a60a9586c44d20e8915c88d3d56f4a92e95b44b4
Programmer Rakibul <rakibul00206@gmail.com>
2025-09-08 18:31:58 +0600

    Plants Details image ratio added

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ const displayDetails = (details) => {
 
   detailsBox.innerHTML = `
       <h4 class="font-bold text-2xl">${name}</h4>
-      <img src="${image}" class="rounded-lg"/>
+      <img src="${image}" class="rounded-lg aspect-square object-cover"/>
       <span class="block"><strong>Category: </strong>${category}</span>
       <span class="block"><strong>Price: </strong>$${price}</span>
       <p><strong>Description: </strong>${description}</p>
